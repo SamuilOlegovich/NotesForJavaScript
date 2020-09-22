1c9da90280a2ece3620f067fe274815e66d0cc63
SamuilOlegovich <47562089+SamuilOlegovich@users.noreply.github.com>
2020-09-22 22:29:31 +0300

    ADD

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // DOM
+alert("!");
+window.alert("!");  // одно и то же
+window.confirm("Это ты?");
+window.prompt("Как вас зовут?");
+
+const heading = document.getElementById("hello");
+console.log(heading);
+console.dir(heading); // можно раскрывать и смотреть дериктории, а не просто вывод
